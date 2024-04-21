fde85c4e48b4a74123d352166d12ee59064d3d06
Michal Macko <knowledgehole@gmail.com>
2024-04-21 21:18:06 +0200

    added rotation to the animation that fits the idea of rolling

--- generator.js
+++ generator.js
@@ -1,8 +1,8 @@
 document.querySelector('.dice').addEventListener('click', function() {
     this.classList.toggle('clicked');
-    if (this.classList.contains('clicked')) {
-        setTimeout(() => {
-            this.classList.remove('clicked');
-        }, 100); // Toggle 'active' class after 100ms
-    }
+        if (this.classList.contains('clicked')) {
+            setTimeout(() => {
+                this.classList.remove('clicked');
+            }, 200); // Toggle 'active' class after 100ms
+        }
 });
