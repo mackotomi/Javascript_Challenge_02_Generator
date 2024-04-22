28b7dc0e0a207451391c2908d3858af84ee30e58
Michal Macko <knowledgehole@gmail.com>
2024-04-22 18:11:25 +0200

    working on the javascript code

--- generator.js
+++ generator.js
@@ -6,3 +6,18 @@ document.querySelector('.dice').addEventListener('click', function() {
             }, 200); // Toggle 'active' class after 100ms
         }
 });
+
+
+
+let keyrange = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0';
+
+let keys = [keyrange.split(" ")];
+
+console.log(keys);
+
+// Variable with total number of keys in array.
+let keyLength = keys.length;
+let keyZero = 0;
+
+var randomNumber = Math.floor(Math.random() * keys.length) + keyZero;
+console.log(randomNumber);
---
+++ test.js
@@ -0,0 +1,8 @@
+console.log('new script begins after his sentence');
+
+let sentence = 'this is sentence with all kinds of text. Here is another sentence with another text and some separated alphabets like the A,b,C,d now E,f,g,h,I';
+
+console.log(sentence);
+
+let splitted = sentence.split(' ');
+console.log(splitted);
