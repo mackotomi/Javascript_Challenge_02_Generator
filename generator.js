330f6120f8ba25f76369c9b89c3df8fd48d8f22c
Michal Macko <knowledgehole@gmail.com>
2024-04-22 20:23:56 +0200

    added comments for description

--- generator.js
+++ generator.js
@@ -1,3 +1,4 @@
+// Animation trigger for dice roll if clicked for representing the roll for random new key.
 document.querySelector('.dice').addEventListener('click', function() {
     this.classList.toggle('clicked');
         if (this.classList.contains('clicked')) {
@@ -7,16 +8,19 @@ document.querySelector('.dice').addEventListener('click', function() {
         }
 });
 
-
-
+// This variable contains all letter and numbers with lowercase and uppercase format.
 let keyrange = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0';
 
+// The keyrange variable is splitted into separated indexes into array with name keys.
 let keys = [keyrange.split(" ")];
 
+// Check of the split.
 console.log(keys);
 
-// Variable with total number of keys in array.
+// Variable of total number count or length based on array index count.. also represinting the max number for math random method.
 let keyLength = keys.length;
+
+// Variable with 0 number that is the minimum number for the random math, the idea is to generate random number from 0 to max number based on the array count of keyLength.
 let keyZero = 0;
 
 var randomNumber = Math.floor(Math.random() * keys.length) + keyZero;
